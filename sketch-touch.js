25e1fecf3ac632da1b07394451f1284ebdcb16c0
Chris Ward <cw@yellowdave.co.uk>
2023-03-22 18:48:13 +0000

    simplify touch example

--- sketch-touch.js
+++ sketch-touch.js
@@ -1,33 +1,20 @@
-let valueY;
+let dim = 300;
 
 function setup() {
-  // Create Canvas of size 500*500
-  createCanvas(500, 500);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   // set background color
-  background(200);
-  fill("green");
-  // set text and text size
-  textSize(25);
-  text("Touch to change color", 30, 30);
-  // fill color according to mouseMoved()
-  fill(valueX, 255 - valueY, 255 - valueX);
-  // draw rectangle
-  rect(mouseX, mouseY, 115, 115);
-  fill(valueY, 255 - valueX, 255 - valueX);
+  background("pink");
 
-  rect(mouseX, mouseY + 115, 115, 115);
-  fill(255 - valueY, 255 - valueX, 255 - valueY);
-
-  rect(mouseX - 115, mouseY, 115, 115);
-  fill(255 - valueY, 255 - valueY, 255 - valueY);
-
-  rect(mouseX - 115, mouseY + 115, 115, 115);
+  if (touches.length === 1) {
+    fill(255);
+    noStroke();
+    circle(mouseX, mouseY, dim);
+  }
 }
 
 function touchStarted() {
-  valueX = mouseX % 255;
-  valueY = mouseY % 255;
+  return false;
 }
