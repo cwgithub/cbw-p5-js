dff1440d26f5f1542739844665b30c0d88380ea5
Chris Ward <cw@yellowdave.co.uk>
2023-03-28 18:49:24 +0100

    Added Speech recognition.

---
+++ sketch-speech.js
@@ -0,0 +1,15 @@
+let video;
+function setup() {
+  noCanvas();
+  let lang = navigator.language || "en-US";
+  let speechRec = new p5.SpeechRec(lang, gotSpeech);
+  speechRec.start();
+
+  function gotSpeech() {
+    if (speechRec.resultValue) {
+      createP(speechRec.resultString);
+    }
+
+    console.log(speechRec);
+  }
+}
