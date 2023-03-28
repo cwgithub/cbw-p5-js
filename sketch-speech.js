fcfbd11999129f2f687a33bb769c6d663e8f2a39
Chris Ward <cw@yellowdave.co.uk>
2023-03-28 18:59:58 +0100

    continuous, interim

--- sketch-speech.js
+++ sketch-speech.js
@@ -3,7 +3,10 @@ function setup() {
   noCanvas();
   let lang = navigator.language || "en-US";
   let speechRec = new p5.SpeechRec(lang, gotSpeech);
-  speechRec.start();
+
+  let continuous = true;
+  let interim = false;
+  speechRec.start(continuous, interim);
 
   function gotSpeech() {
     if (speechRec.resultValue) {
