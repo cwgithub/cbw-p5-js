531066ed3dffdfd87db7f8cda1ad2704ad6787b6
Chris Ward <cw@yellowdave.co.uk>
2023-04-10 18:10:45 +0100

    more deep-dive work into the pixels array and pixelDensity

--- models/pixels-helper.js
+++ models/pixels-helper.js
@@ -23,18 +23,55 @@ class PixelHelper {
     this._imageWidth = imageWidth;
     this._imageHeight = imageHeight;
     this._pxDensity = pxDensity;
+
+    console.log("======================================");
+    console.log(
+      "_pixelsMatrix  : " +
+        this._pixelsMatrix.length +
+        " (pixels : " +
+        this._pixelsMatrix.length / 4 +
+        ")"
+    );
+    console.log("_imageWidth    : " + this._imageWidth);
+    console.log("_imageHeight   : " + this._imageHeight);
+    console.log("_pxDensity     : " + this._pxDensity);
+    console.log("======================================");
   }
 
   getPixel(x, y) {
-    const yOffset = 4 * y * this._imageWidth;
-    const pixelStart = yOffset + x * 4;
+    let r, g, b, a;
 
-    return new pixel(
-      this._pixelsMatrix[pixelStart + 0],
-      this._pixelsMatrix[pixelStart + 1],
-      this._pixelsMatrix[pixelStart + 2],
-      this._pixelsMatrix[pixelStart + 3]
-    );
+    for (let i = 0; i < this._pxDensity; i++) {
+      for (let j = 0; j < this._pxDensity; j++) {
+        const index =
+          4 *
+          ((y * this._pxDensity + j) * width * this._pxDensity +
+            (x * this._pxDensity + i));
+        r = this._pixelsMatrix[index];
+        g = this._pixelsMatrix[index + 1];
+        b = this._pixelsMatrix[index + 2];
+        a = this._pixelsMatrix[index + 3];
+        console.log(
+          `-- ${x}, ${y} ----- ${i}, ${j} ------------------------------------`
+        );
+        console.log(`${index} r = ${r}`);
+        console.log(`${index + 1} r = ${g}`);
+        console.log(`${index + 2} r = ${b}`);
+        console.log(`${index + 3} r = ${a}`);
+      }
+    }
+
+    return new pixel(r, g, b, a);
+
+    // const yOffset = 4 * (y * this._pxDensity) * this._imageWidth;
+    // const pixelStart = yOffset + x * this._pxDensity * 4;
+
+    // return new pixel(
+    //   this._pixelsMatrix[pixelStart + 0],
+    //   this._pixelsMatrix[pixelStart + 1],
+    //   this._pixelsMatrix[pixelStart + 2],
+    //   this._pixelsMatrix[pixelStart + 3]
+    // );
   }
 }
 
--- pixels/sketch-pixels.js
+++ pixels/sketch-pixels.js
@@ -14,121 +14,71 @@ function drawShape(x, y, size) {
 }
 
 function setup() {
-  pixelDensity(1);
-  createCanvas(300, 300);
+  // pixelDensity(1);
+
+  const size = 1;
+
+  createCanvas(size * 3, size * 3);
   background(123);
 
   strokeWeight(0);
   fill([0, 255, 0, 255]);
-  drawShape(0, 0, 100);
+  drawShape(0, 0, size);
 
   fill([255, 0, 0, 255]);
-  drawShape(100, 0, 100);
+  drawShape(size, 0, size);
 
   fill([0, 0, 255, 255]);
-  drawShape(0, 100, 100);
+  drawShape(0, size, size);
 
   fill([255, 255, 255, 255]);
-  drawShape(100, 100, 100);
+  drawShape(size, size, size);
 
   loadPixels();
 
   const ph = new PixelHelper(pixels, width, height, pixelDensity());
 
-  console.log("Green");
   console.log(ph.getPixel(0, 0));
-  console.log(ph.getPixel(99, 99));
-  console.log(ph.getPixel(200, 200));
 
-  console.log("Red");
-  console.log(ph.getPixel(100, 0));
-  console.log(ph.getPixel(199, 99));
+  // console.log("Background");
+  // console.log(ph.getPixel(size * 2, 0));
+  // console.log(ph.getPixel(size * 2, size * 2));
+  // console.log(ph.getPixel(0, size * 2));
 
-  console.log("White");
-  console.log(ph.getPixel(100, 100));
-  console.log(ph.getPixel(199, 199));
+  // console.log("Green");
+  // console.log(ph.getPixel(0, 0));
+  // console.log(ph.getPixel(size - 1, size - 1));
 
-  console.log("Blue");
-  console.log(ph.getPixel(0, 100));
-  console.log(ph.getPixel(99, 199));
+  // console.log("Red");
+  // console.log(ph.getPixel(size, 0));
+  // console.log(ph.getPixel(size * 2 - 1, size - 1));
 
-  return;
+  // console.log("White");
+  // console.log(ph.getPixel(size, size));
+  // console.log(ph.getPixel(size * 2 - 1, size * 2 - 1));
 
-  for (let y = 0; y < height; y++) {
-    const offset = y * width * 4;
-    console.log("----- offset = " + offset);
-    for (let x = 0; x < width; x++) {
-      let pix = offset + x * 4;
-      console.log(pix + " = " + 0);
-      pixels[pix] = 0;
-      pix = offset + x * 4 + 1;
-      console.log(pix + " = " + 255);
-      pixels[pix] = 255;
-      pix = offset + x * 4 + 2;
-      console.log(pix + " = " + 0);
-      pixels[pix] = 0;
-      pix = offset + x * 4 + 3;
-      console.log(pix + " = " + 255);
-      pixels[pix] = 255;
-    }
-  }
+  // console.log("Blue");
+  // console.log(ph.getPixel(0, size));
+  // console.log(ph.getPixel(size - 1, size * 2 - 1));
+
+  // for (let y = 0; y < height; y++) {
+  //   const offset = y * width * 4;
+  //   console.log("----- offset = " + offset);
+  //   for (let x = 0; x < width; x++) {
+  //     let pix = offset + x * 4;
+  //     console.log(pix + " = " + 0);
+  //     pixels[pix] = 0;
+  //     pix = offset + x * 4 + 1;
+  //     console.log(pix + " = " + 255);
+  //     pixels[pix] = 255;
+  //     pix = offset + x * 4 + 2;
+  //     console.log(pix + " = " + 0);
+  //     pixels[pix] = 0;
+  //     pix = offset + x * 4 + 3;
+  //     console.log(pix + " = " + 255);
+  //     pixels[pix] = 255;
+  //   }
+  // }
 
   updatePixels();
-
-  // pixelDensity(1);
-
-  // strokeWeight(0);
-  // fill("pink");
-  // rect(10, 10, 180, 180);
-
-  // deleteMe(createVector(100, 100));
-
-  // floodFill(createVector(100, 100), [0, 255, 0, 255]);
-
-  // strokeWeight(1);
-  // ellipse(100, 100, 4, 4);
 }
-
-// function setup() {
-//   const size = 10;
-
-//   createCanvas(size, size);
-
-//   console.log("displayDensity : " + displayDensity());
-//   console.log("pixelDensity   : " + pixelDensity());
-
-//   pixelDensity(displayDensity());
-//   console.log("pixelDensity   : " + pixelDensity());
-//   pixelDensity(1);
-//   console.log("pixelDensity   : " + pixelDensity());
-//   background(123);
-
-//   strokeWeight(0);
-
-//   fill("red");
-//   quad(0, 0, size, 0, size, size, 0, size);
-
-//   // fill("green");
-//   // quad(0, size + 1, size, size + 1, size * 2, size * 2, 0, size * 2);
-
-//   // fill("white");
-//   // quad(size + 1, 0, size * 2, 0, size * 2, size, size + 1, size);
-
-//   // fill("blue");
-//   // quad(
-//   //   size + 1,
-//   //   size + 1,
-//   //   size * 2,
-//   //   size + 1,
-//   //   size * 2,
-//   //   size * 2,
-//   //   size + 1,
-//   //   size * 2
-//   // );
-
-//   loadPixels();
-
-//   console.log(pixels);
-// }
-
-// function draw() {}
